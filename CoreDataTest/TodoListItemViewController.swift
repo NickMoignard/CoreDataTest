970a753dc44b695050ed75509d13ba181f71476e
Nicholas Moignard <nicklmoignard@gmail.com>
2017-05-04 10:21:31 +1000

    initial commit

---
+++ CoreDataTest/TodoListItemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListItemViewController.swift
+//  CoreDataTest
+//
+//  Created by Alice Newman on 4/5/17.
+//  Copyright © 2017 Nicholas Moignard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class TodoItemViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ CoreDataTest/TodoListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListViewController.swift
+//  CoreDataTest
+//
+//  Created by Alice Newman on 4/5/17.
+//  Copyright © 2017 Nicholas Moignard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class TodoListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
